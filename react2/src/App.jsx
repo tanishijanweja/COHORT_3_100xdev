a4cea3b9668d381b115e40bb18ec697a86ec4a47
Tanishi <tanishi.janweja@gmail.com>
2025-04-03 20:59:04 +0530

    new commit

---
+++ react2/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Link to="/">ALLEN</Link>|
+        <Link to="/neet/online-coaching-class-11">Class 11</Link>|
+        <Link to="/neet/online-coaching-class-12">Class 12</Link>
+        <Routes>
+          <Route
+            path="/neet/online-coaching-class-11"
+            element={<Class11Program />}
+          />
+          <Route
+            path="/neet/online-coaching-class-12"
+            element={<Class12Program />}
+          />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function Landing() {
+  return <div> Welcome to ALLEN</div>;
+}
+
+function Class11Program() {
+  return <div>NEET programs for Class 11th</div>;
+}
+
+function Class12Program() {
+  return <div>NEET programs for Class 12th</div>;
+}
+
+export default App;
