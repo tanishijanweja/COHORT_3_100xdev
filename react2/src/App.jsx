030e6f05c7bacebb73d608d6630659ab8541eaaf
Tanishi <tanishi.janweja@gmail.com>
2025-04-07 10:44:10 +0530

    new commit

--- react2/src/App.jsx
+++ react2/src/App.jsx
@@ -1,39 +1,32 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React, { useEffect, useRef, useState } from 'react';
 
-function App() {
+function Chat() {
+  const [messages, setMessages] = useState(["Hello!", "How are you?"]);
+  const chatBoxRef = useRef(null);
+
+  // Function to simulate adding new messages
+  const addMessage = () => {
+    setMessages((prevMessages) => [...prevMessages, "New message!"]);
+  };
+
+  // Scroll to the bottom whenever a new message is added
+  useEffect(() => {
+    chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+  }, [messages]);
+
   return (
     <div>
-      <BrowserRouter>
-        <Link to="/">ALLEN</Link>|
-        <Link to="/neet/online-coaching-class-11">Class 11</Link>|
-        <Link to="/neet/online-coaching-class-12">Class 12</Link>
-        <Routes>
-          <Route
-            path="/neet/online-coaching-class-11"
-            element={<Class11Program />}
-          />
-          <Route
-            path="/neet/online-coaching-class-12"
-            element={<Class12Program />}
-          />
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </BrowserRouter>
+      <div 
+        ref={chatBoxRef} 
+        style={{ height: "200px", overflowY: "scroll", border: "1px solid black" }}
+      >
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </div>
+      <button onClick={addMessage}>Add Message</button>
     </div>
   );
 }
 
-function Landing() {
-  return <div> Welcome to ALLEN</div>;
-}
-
-function Class11Program() {
-  return <div>NEET programs for Class 11th</div>;
-}
-
-function Class12Program() {
-  return <div>NEET programs for Class 12th</div>;
-}
-
-export default App;
+export default Chat;
