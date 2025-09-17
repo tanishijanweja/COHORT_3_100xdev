7f3d5e1fe99539f81228eab09aca83b915be6ca0
tanishijanweja <tanishi.janweja@gmail.com>
2025-09-17 15:47:14 +0530

    database setup

---
+++ second_brain/dist/middleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config.js";
+export const userMiddleware = (req, res, next) => {
+    const header = req.headers["authorization"];
+    const decoded = jwt.verify(header, JWT_PASSWORD);
+    if (decoded) {
+        //@ts-ignore
+        req.userId = decoded.id; //how to overwrite the types of the express request object
+        next();
+    }
+    else {
+        res.status(403).json({
+            message: "You're not logged in",
+        });
+    }
+};
+//# sourceMappingURL=middleware.js.map
